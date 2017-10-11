3bb9c299412a75a35bc423d1ef4ed059f3023de0
Sami Karjalainen <sami.karjalainen@opuscapita.com>
2017-10-11 10:38:49 +0300

    Add 500ms delay to spinner, upgrade packages

--- src/spinner/spinner.component.jsx
+++ src/spinner/spinner.component.jsx
@@ -1,13 +1,37 @@
+/* Based on from https://github.com/qimingweng/react-spinjs */
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
-import ReactSpinner from 'react-spinjs';
+import PropTypes from 'prop-types';
+import SpinJS from 'spin.js';
 
+export default class Spinner extends React.PureComponent {
+  static propTypes = {
+    config: PropTypes.object,
+    delay: PropTypes.number.isRequired,
+  }
 
-export default function Spinner() {
-  const spinnerOptions = {
-    color: '#FAC51D',
-    width: 4,
-  };
-  return (
-    <ReactSpinner config={spinnerOptions} />
-  );
+  static defaultProps = {
+    config: {
+      color: '#FAC51D',
+      width: 4,
+    },
+    delay: 500,
+  }
+
+  componentDidMount() {
+    window.setTimeout(() => {
+      this.spinner = new SpinJS(this.props.config);
+      this.spinner.spin(this.container);
+    }, this.props.delay);
+  }
+
+  componentWillUnmount() {
+    if (this.spinner) {
+      this.spinner.stop();
+    }
+  }
+
+  render() {
+    return <span ref={(el) => { this.container = el; }} />;
+  }
 }
