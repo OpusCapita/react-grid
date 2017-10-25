f721ad580ec354bdd3450f34cc85875d761d4b88
Sami Karjalainen <sami.karjalainen@opuscapita.com>
2017-10-25 10:54:06 +0300

    Test build

--- webpack/base.release.config.js
+++ webpack/base.release.config.js
@@ -5,11 +5,6 @@ function getBaseEnvConfiguration(config) {
   return {
     devtool: 'source-map',
     plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      }),
       new webpack.optimize.ModuleConcatenationPlugin(),
     ],
   };
